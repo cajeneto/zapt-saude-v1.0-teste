08f2ee9141ad33a5288b4bfd29a8d5ef7b452b63
Cajé Neto <89559306+cajeneto@users.noreply.github.com>
2023-11-06 20:50:05 -0300

    regPatient.js v1.2

--- assets/js/regPatient.js
+++ assets/js/regPatient.js
@@ -5,6 +5,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebas
 //import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
 import { getFirestore, collection, getDocs} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js"
 import { getDatabase, set, get, update, remove, ref, child} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-database.js"
+//import { registerLog } from "./_DATE_PATIENT";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -120,16 +121,19 @@ function criaDivfilho() {
 
 function btnEditarReg(){
    var btnEditReg = document.getElementById('btnEditReg').addEventListener('click', ()=> {
+      //EditDados();
    });
    
 }
 
-function btnImprimeReg(){
-   var btnImprimeReg = document.getElementById('btnImprimeReg').addEventListener('click', ()=> {
-      alert('ola mundo imprime')
+/* function btnImprimeReg(){
+   var btnImprimeReg = document.getElementById('btnImprimeReg').addEventListener('click', (e)=> {
+      e.preventDefault();
+         alert('ola mundo imprime')
+
    });
    
-}
+} */
 
 
 /* function criaElementoReg(){
@@ -180,11 +184,11 @@ function btnImprimeReg(){
 
 
    
-   // Função para listar o array
+   // Função para listar o array que está dentro da collection
    function listarArray() {
       const dbref = ref(db);
       const collectionName = 'pessoa/';
-      const id_user = ''
+      //const id_user = ''
       get(child(dbref, collectionName))
       .then((snapshot) => {
             snapshot.forEach((childSnapshot) => {
@@ -199,112 +203,196 @@ function btnImprimeReg(){
                var dataNascimento = childSnapshot.val().crianca.dataNascimento; 
                var rg = childSnapshot.val().crianca.rg;
 
-               console.log(criancaCol)
-               console.log(infoRegistro)
-               console.log(responsavelCol)
-               console.log(cartaoSus)
-               console.log(contato)
-               console.log(cpfCrianca)
-               console.log(dataNascimento)
-               console.log('--------------------------------')
 
                
-const insereDiv = `
-<div class="qntPac">
-   <div class="nomePacListReg"><strong>NOME DA CRIANÇA: <br></strong>${criancaCol}</div>
-   <div class="idPacReg"><strong>ID Registro: <br></strong>${infoRegistro}</div>
-   <div class="cpfCriaReg"><strong> CPF: <br></strong>${cpfCrianca}</div>
-   <div class="ResponsavelCriaReg"><strong>DATA NASCIMENTO: <br></strong>${dataNascimento}</div>
-   <div class="dnCriaReg"><strong> RESPONSÁVEL:<br></strong>${responsavelCol}</div>
-   <div class="SUSReg"><strong> CARTÃO SUS:<br></strong>${cartaoSus}</div>
-   <div class="TelefoneReg"><strong> CONTATO: <br></strong>${contato}</div>
-<div class="buttonEdit">
-   <button id="btnEditReg">Editar</button>
-</div>
-<div class="buttonImprime">
-   <button id="btnImprimeReg">Imprimir</button>
-</div>
-</div>
-`
-function criaDivfilho() {
-   const reg1 = document.querySelector('.pacCad')
-   reg1.innerHTML+=insereDiv
-   btnEditarReg();
-   btnImprimeReg();
-   //getDataPessoa();
-   //criaElementoReg();
+               
+               // Função para criar e retornar a div com as informações
+function criarDiv(infoRegistro, criancaCol, cpfCrianca, responsavelCol, dataNascimento, cartaoSus, contato) {
+   const div = document.createElement('div');
+   div.classList.add('qntPac');
+   div.id = `qtdPac-${infoRegistro}`;
+
+   div.innerHTML = `
+       <div class="nomePacListReg"><strong>NOME DA CRIANÇA: <br></strong>${criancaCol}</div>
+       <div class="idPacReg"><strong>ID Registro: <br></strong>${infoRegistro}</div>
+       <div class="cpfCriaReg"><strong> CPF: <br></strong>${cpfCrianca}</div>
+       <div class="dnCriaReg"><strong> RESPONSÁVEL:<br></strong>${responsavelCol}</div>
+       <div class="ResponsavelCriaReg"><strong>DATA NASCIMENTO: <br></strong>${dataNascimento}</div>
+       <div class="SUSReg"><strong> CARTÃO SUS:<br></strong>${cartaoSus}</div>
+       <div class="TelefoneReg"><strong> CONTATO: <br></strong>${contato}</div>
+       <div class="buttonEdit">
+           <button class="btnEditReg">Editar</button>
+       </div>
+       <div class="buttonImprime">
+           <button class="btnImprimeReg">Imprimir</button>
+       </div>
+   `;
+
+   const btnEdit = div.querySelector('.btnEditReg');
+    const btnImprime = div.querySelector('.btnImprimeReg');
+
+   // Adicionar listeners para os botões dentro da div criada
+   div.querySelector('.btnEditReg').addEventListener('click', () => {
+       //console.log('Esse é o botão de editar do ID ' + infoRegistro);
+       // Aqui você pode adicionar a lógica para editar com o ID infoRegistro
+   });
+
+   div.querySelector('.btnImprimeReg').addEventListener('click', () => {
+      // console.log('Esse é o botão de imprimir o ID ' + infoRegistro);
+       // Aqui você pode adicionar a lógica para imprimir com o ID infoRegistro
+       acaoBotaoImprime(infoRegistro);
+   });
+
+   return div;
 }
-criaDivfilho();
 
-               //btnImprimeReg();
-               //var nomePacListReg = document.querySelector('.nomePacListReg').innerHTML=criancaCol;
-               //var idPacReg = document.querySelector('.idPacReg').innerHTML="<strong>ID Registro: <br></strong>"+infoRegistro;
-               //var cpfCriaReg = document.querySelector('.cpfCriaReg').innerHTML="<strong> CPF: <br></strong>"+cpfCrianca;
-               //var dnCriaReg = document.querySelector('.dnCriaReg').innerHTML=" <strong>DATA NASCIMENTO: <br></strong>"+dataNascimento;
-               //dados responsável
-               //var ResponsavelCriaReg = document.querySelector('.ResponsavelCriaReg').innerHTML="<strong> RESPONSÁVEL:<br></strong> "+responsavelCol;
-               //var SUSReg = document.querySelector('.SUSReg').innerHTML="<strong> CARTÃO SUS:<br></strong> "+cartaoSus;
-               //var TelefoneReg = document.querySelector('.TelefoneReg').innerHTML="<strong> CONTATO: <br></strong> "+contato;
-               //console.log(childKey)
-               //console.log(childData)
-               
-
-
-
-               //console.log(`${childKey}: ${JSON.stringify(childData)}`);
-            
+function acaoBotaoImprime(id) {
+   if (id === infoRegistro) {
+       //console.log('Ação de impressão para o ID '+infoRegistro);
+      const conteudo = `
+      <h2 style='text-align:center'>Dados da Criança</h2>
+      <div style="background-image: url('/assets/img/zapt-saude-logo.jpeg'); width: 500px; height: 200px;"></div>
+      <hr/>
+      <h4 style='text-align:start'>Registro Nº: ${infoRegistro}</h4>
+      <hr/>
+      <h4 style='text-align:start'>Nome da Criança: ${criancaCol}</h4>
+      <h4 style='text-align:start'>CPF: ${cpfCrianca}</h4>
+      <h4 style='text-align:start'>Data de nascimento: ${dataNascimento}</h4>
+      <h4 style='text-align:start'>Cartão Sus: ${cartaoSus}</h4>
+      <h4 style='text-align:start'>Contato: ${contato}</h4>
+      <h4 style='text-align:start'>Responsável: ${responsavelCol}</h4>`;
    
-       })
-      })
-       .catch((error) => {
-         console.error('Erro ao listar o array:', error);
-       });
+
+      let estilo = "<style>";
+      estilo += "body{font-family:'Helvetica Neue', Helvetica, Arial, sans-serif;}";
+      estilo += "</style>";
+      const win = window.open('','', 'height=700,width=700');
+      win.document.write('<html><head>');
+      win.document.write('<title>ZAPT SAÚDE</title>');
+      win.document.write(estilo);
+      win.document.write('</head>');
+      win.document.write('<body>');
+      win.document.write(conteudo);
+      win.document.write('</body></html>');
+      win.print()
+      win.close()
+      
+
+
+
+       // Coloque aqui a lógica para imprimir o item com o ID 123
+   } else {
+       console.log('Ação de impressão para outro ID');
+       // Outra ação ou lógica para IDs diferentes
+   }
+}
+
+function criaDivFilho() {
+   // Suponhamos que você tenha variáveis com os valores necessários
+   const infoRegistro1 = 'infoRegistro';
+   const criancaCol1 = criancaCol;
+   const cpfCrianca1 = '123.456.789-00';
+   const responsavelCol1 = 'Maria';
+   const dataNascimento1 = '01/01/2010';
+   const cartaoSus1 = '1234567890';
+   const contato1 = '987654321';
+
+   const reg1 = document.querySelector('.pacCad');
+   reg1.appendChild(criarDiv(infoRegistro, criancaCol, cpfCrianca, responsavelCol, dataNascimento, cartaoSus, contato));
+}
+
+
+
+criaDivFilho();
+
+                  
+
+                  
+                 
+                  
+               })
+            })
+            .catch((error) => {
+               console.error('Erro ao listar o array:', error);
+            });
    }
 
+   listarArray();
+
    
+   
+   
+   
+   //btNew();
    // Chame a função para listar o array
-   //listarArray();
-
-
-
-
+   
    //function obterRegistros5Min() {
       // Defina a lógica para obter registros aqui
       //console.log('Obtendo registros a cada 1 minuto...');
       //listarArray()
-    //}
-    
-    // Defina um intervalo de 5 minutos (5 * 60 * 1000 milissegundos)
-    //const intervalo = 1 * 60 * 1000;
-    
-    // Execute a função a cada 5 minutos
-    //setInterval(obterRegistros5Min, intervalo);
-    
-    // Chame a função uma vez imediatamente para iniciar o processo
+      //}
+      
+      // Defina um intervalo de 5 minutos (5 * 60 * 1000 milissegundos)
+      //const intervalo = 1 * 60 * 1000;
+      
+      // Execute a função a cada 5 minutos
+      //setInterval(obterRegistros5Min, intervalo);
+      
+      // Chame a função uma vez imediatamente para iniciar o processo
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      /* window.onload = () => {
+         //getDataPessoa();
+         //criaDivfilho();
+         //listarArray()
+         //obterRegistros5Min();
+         //EditDados()
+         
+      } */
+      
+      
+         
+      
+
+
+// função para editar dados
+function EditDados(){
+
+   const collectionName = 'pessoa/';
+   const trocaNome = 'teste troca de nome 11'
+   
+      update(ref(db, collectionName +'12/crianca'), {
+         nomeCompletoCrianca: trocaNome,
+         
+      })
+      .then(()=>{
+         alert("Atualizado com sucesso!");
+         //window.location.reload();
+         }).catch((error)=>{
+            console.error(`Error: ${error}`);
+            });
+            
+   }
+  //EditDados()
+
+  
 
 
 
 
+ 
 
 
 
 
-
-
-
-window.onload = () => {
-   //getDataPessoa();
-   //criaDivfilho();
-   listarArray()
-   //obterRegistros5Min();
-}
-
-
-
-
-
-
-
+// getHoss
 
 
 
